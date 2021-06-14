2eaf9784c38f39f6f73682ca5a4b73a9f75e36d1
rlrpara <rlr.para@gmail.com>
2021-06-14 16:52:55 -0300

    criação dos projetos

---
+++ PortalNoticiasProject/PortalNoticias.Test/Application.Services/UserServicesTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PortalNoticias.Test.Application.Services
+{
+    public class UserServicesTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
