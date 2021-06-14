be543922a0f30495e2fb02d6befa86239dd6b555
rlrpara <rlr.para@gmail.com>
2021-06-14 19:36:29 -0300

    ajuste de estrutura

--- PortalNoticiasProject/PortalNoticias.Api/Startup.cs
+++ PortalNoticiasProject/PortalNoticias.Api/Startup.cs
@@ -1,34 +1,28 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using PortalNoticias.Infra.CrossCutting.IoC;
 
 namespace PortalNoticias.Api
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
             services.AddControllers();
+            NativeInjector.RegisterServices(services);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
---
+++ PortalNoticiasProject/PortalNoticias.Infra.CrossCutting.IoC/NativeInjector.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PortalNoticias.Application.Interfaces;
+using PortalNoticias.Application.Services;
+using PortalNoticias.Domain.Interfaces;
+using PortalNoticias.Infra.Data.Repositories;
+
+namespace PortalNoticias.Infra.CrossCutting.IoC
+{
+    public static class NativeInjector
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<IBaseService, BaseService>();
+            services.AddScoped<IBaseRepository, BaseRepository>();
+        }
+    }
+}
